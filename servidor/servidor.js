91d525a8abeddf9e0c028a465488154dc90a1610
KennedyRibeiro <kennedydeveloper1@gmail.com>
2023-11-14 19:55:00 -0300

    Controle de usuários feito, node.js

---
+++ servidor/servidor.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static('cliente', {'extensions': ['html'] }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({exteded: true}));
+
+//console.log(__dirname);
+//console.log(path.join(__dirname, '..', 'cliente', 'index.html'));
+
+const diretorioHtml = path.join(__dirname, '..', 'cliente', 'index.html');
+
+app.get('/', (req, res) => {
+    res.contentType('.html');
+    res.sendFile(diretorioHtml);
+});
+
+
+const PORTA = 3000;
+
+app.listen(PORTA, () => {
+    console.log(`Servidor em execução na porta ${PORTA}`);
+});
+
